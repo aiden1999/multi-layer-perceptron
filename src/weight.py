07d3c98813e0a139b568a7a4a59b37f18735bb81
Aiden Tempest <aidentempest99@gmail.com>
2026-05-13 17:59:28 +0100

    init for weight

---
+++ src/weight.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+from src.node import Node
+
+
+class Weight:
+    def __init__(self, left_node: Node, right_node: Node, node_count: int):
+        """[TODO:description]
+
+        Args:
+            left_node: [TODO:description]
+            right_node: [TODO:description]
+            node_count: the number of input nodes if left_node is an input node, or the number of hidden nodes if left_node is a hidden node.
+        """
+        self.left_node = left_node
+        self.right_node = right_node
+        self.value = np.random.uniform(-2 / node_count, 2 / node_count)
+        self.value_old = None
