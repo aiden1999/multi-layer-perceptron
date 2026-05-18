7096de0d13ae833a33f1d1510997079671d1fc5c
Aiden Tempest <aidentempest99@gmail.com>
2026-05-18 13:42:54 +0100

    added logging to mlp.py

--- src/mlp.py
+++ src/mlp.py
@@ -2,18 +2,25 @@ import argparse
 import numpy as np
 
 from src.ingest_data import get_datasets
+from src.logger import setup_logger
 from src.perceptron import Perceptron
 
 # from src.testing import test
 
 
 def main(*, num_nodes=6, step_size=0.1, use_momentum=True):
+    logger = setup_logger("mlp", "mlp.log")
+
+    logger.info("Getting data sets")
     training_data, validation_data, testing_data = get_datasets()
     num_inputs = np.shape(training_data)[1] - 1
 
+    logger.info("Creating perceptron")
     perceptron = Perceptron(
         num_inputs, num_nodes, training_data, use_momentum, step_size, validation_data
     )
+
+    logger.info("Training perceptron")
     perceptron.train()
     # test()
 
