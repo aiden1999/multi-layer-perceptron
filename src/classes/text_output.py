62ead91708c6384abd4a793ff408dc8f7193bd31
Aiden Tempest <aidentempest99@gmail.com>
2026-06-24 13:14:39 +0100

    added stuff to output

--- src/classes/text_output.py
+++ src/classes/text_output.py
@@ -11,10 +11,15 @@ class TextOutput:
         self.used_momentum = perceptron.use_momentum
         self.used_bold_driver = perceptron.use_bold_driver
         self.used_annealing = perceptron.use_annealing
-        # epoch count
-        # rmses
-        # nodes with biases
-        # weights and the nodes between
+        self.epoch_count = perceptron.epoch_count
+        self.rmse_training = perceptron.rmse_training
+        self.rmse_validation = perceptron.rmse_validation
+        self.rmse_testing = perceptron.rmse_testing
+        self.input_nodes = perceptron.input_nodes
+        self.hidden_nodes = perceptron.hidden_nodes
+        self.output_node = perceptron.output_node
+        self.weights_ih = perceptron.weights_ih
+        self.weights_ho = perceptron.weights_ho
         # data used in each set.
 
     def write_to_json(self):
@@ -26,11 +31,36 @@ class TextOutput:
 
     def _create_output(self):
         output = {
-            "number_of_hidden_nodes": self.num_nodes,
-            "step_size": self.step_size,
-            "used_momentum": self.used_momentum,
-            "used_bold_driver": self.used_bold_driver,
-            "used_annealing": self.used_annealing,
+            "parameters": {
+                "number_of_hidden_nodes": self.num_nodes,
+                "step_size": self.step_size,
+                "used_momentum": self.used_momentum,
+                "used_bold_driver": self.used_bold_driver,
+                "used_annealing": self.used_annealing,
+            },
+            "epoch_count": self.epoch_count,
+            "rmse": {
+                "training": self.rmse_training,
+                "validation": self.rmse_validation,
+                "testing": self.rmse_testing,
+            },
+            "biases": {
+                "hidden_nodes": {},
+                "output_node": self.output_node.bias,
+            },
+            "weights": {"input_to_hidden": {}, "hidden_to_output": {}},
         }
+        for h in self.hidden_nodes:
+            output["biases"]["hidden_nodes"].update({h.index: h.bias})
+            output["weights"]["hidden_to_output"].update(
+                {h.index: self.weights_ho[h.index].value}
+            )
+
+        for i in self.input_nodes:
+            output["weights"]["input_to_hidden"].update({i.index: {}})
+            for h in self.hidden_nodes:
+                output["weights"]["input_to_hidden"][i.index].update(
+                    {h.index: self.weights_ih[i.index][h.index].value}
+                )
 
         return output
