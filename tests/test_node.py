ef247edfb6098ad602c703167a27f15a8a112b98
Aiden Tempest <aidentempest99@gmail.com>
2026-06-29 15:44:48 +0100

    test calculate f prime works

--- tests/test_node.py
+++ tests/test_node.py
@@ -17,3 +17,12 @@ def test_activation_function_works():
     test_node.activation_function()
     actual_u = test_node.u
     assert expected_u == actual_u
+
+
+def test_calculate_f_prime_works():
+    test_node = Node(index=0)
+    test_node.u = 5
+    expected_f_prime = -20
+    test_node.calculate_f_prime()
+    actual_f_prime = test_node.f_prime
+    assert expected_f_prime == actual_f_prime
