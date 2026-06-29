466a4b8bb2e22325e791c26438e0a02f5cc1b6ca
Aiden Tempest <aidentempest99@gmail.com>
2026-06-29 13:22:54 +0100

    test datasets right length

--- tests/test_ingest_data.py
+++ tests/test_ingest_data.py
@@ -2,7 +2,7 @@ import polars as pl
 import polars.testing as pl_testing
 import pytest
 
-from src.ingest_data import standardise_data
+from src.ingest_data import split_data, standardise_data
 
 
 def test_standardise_data_works():
@@ -21,13 +21,29 @@ def test_standardise_data_throws_exception():
         standardise_data(test_df)
 
 
+test_data = {
+    "a": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20],
+    "b": [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20],
+}
+test_df = pl.DataFrame(test_data)
+
+
 def test_split_data_training_df_right_length():
-    pass
+    expected_len = 14
+    split_dfs = split_data(test_df)
+    actual_len = split_dfs[0].shape[0]
+    assert expected_len == actual_len
 
 
 def test_split_data_validation_df_right_length():
-    pass
+    expected_len = 3
+    split_dfs = split_data(test_df)
+    actual_len = split_dfs[1].shape[0]
+    assert expected_len == actual_len
 
 
 def test_split_data_testing_df_right_length():
-    pass
+    expected_len = 3
+    split_dfs = split_data(test_df)
+    actual_len = split_dfs[2].shape[0]
+    assert expected_len == actual_len
