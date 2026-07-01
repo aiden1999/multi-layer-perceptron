ef628f1f3bf1b46e3b37440bc460d9e49ea0cf66
Aiden Tempest <aidentempest99@gmail.com>
2026-07-01 14:27:10 +0100

    test update new value no alpha works

--- tests/test_weight.py
+++ tests/test_weight.py
@@ -12,3 +12,14 @@ def test_update_old_value_works():
     test_weight.update_old_value()
     actual_value_old = test_weight.value_old
     assert expected_value_old == actual_value_old
+
+
+def test_update_new_value_no_alpha_works():
+    test_weight.value = 5
+    step_size = 0.5
+    test_weight.right_node.delta = 4
+    u = 3
+    expected_value = 11
+    test_weight.update_new_value(step_size=step_size, u=u)
+    actual_value = test_weight.value
+    assert expected_value == actual_value
